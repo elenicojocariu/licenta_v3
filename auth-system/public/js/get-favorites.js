2eaafbaea6c8d9d3e579311d0c25ac39fc5a13e1
elenicojocariu <cojocariu.eleni24@gmail.com>
2024-10-03 15:29:12 +0300

    modal-in-favs

--- auth-system/public/js/get-favorites.js
+++ auth-system/public/js/get-favorites.js
@@ -37,12 +37,13 @@ function displayFavoriteArtworks(artworks, userId) {
         return;
     }
     favoritesList.innerHTML = '';
+
     artworks.forEach(artwork => {
         const artworkDiv = document.createElement('div');
         artworkDiv.classList.add('grid-art-item');
         artworkDiv.setAttribute('data-painting-id', artwork.painting_id); // Atribui id-ul picturii
         artworkDiv.innerHTML = `
-            <img src="${artwork.painting_img}" alt="${artwork.painting_name}">
+            <img src="${artwork.painting_img}" alt="${artwork.painting_name}" class="favorite-image" data-title="${artwork.painting_name}" data-img="${artwork.painting_img}">
             <p>${artwork.painting_name}</p>
             <button class="delete-btn" onclick="removeFavoriteFromFavoritesPage('${userId}', '${artwork.painting_id}')"> 
                 <i class="fas fa-trash"></i> 
@@ -50,4 +51,34 @@ function displayFavoriteArtworks(artworks, userId) {
         `;
         favoritesList.appendChild(artworkDiv);
     });
+
+    // Adaugă un event listener pentru a deschide modalul
+    const favoriteImages = document.querySelectorAll('.favorite-image');
+    favoriteImages.forEach(image => {
+        image.addEventListener('click', () => {
+            const modal = document.getElementById('image-modal');
+            const modalImage = document.getElementById('modal-image');
+            const modalTitle = document.getElementById('modal-title');
+
+            modalImage.src = image.dataset.img; // Setează sursa imaginii în modal
+            modalTitle.textContent = image.dataset.title; // Setează numele picturii în modal
+            modal.style.display = 'flex'; // Arată modalul
+
+        });
+    });
+    // Adaugă event listener pentru a închide modalul când utilizatorul apasă pe butonul de închidere
+    const closeButton = document.querySelector('.close-button');
+    closeButton.addEventListener('click', () => {
+        document.getElementById('image-modal').style.display = 'none';
+    });
+
+    // Închide modalul când utilizatorul face clic în afara modalului
+    window.addEventListener('click', (event) => {
+        const modal = document.getElementById('image-modal');
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
 }
+
+
