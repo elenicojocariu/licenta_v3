d424fabc3ce753f874ab466c35599f9326b91897
elenicojocariu <cojocariu.eleni24@gmail.com>
2025-02-05 20:29:01 +0200

    updatess

--- auth-system/controllers/profileController.js
+++ auth-system/controllers/profileController.js
@@ -5,7 +5,7 @@ const multer = require('multer');
 const upload = multer({dest: 'uploads/'});
 
 exports.getProfile = async (req, res) => {
-    const userId = req.user.id; // Id-ul utilizatorului autentificat
+    const userId = req.user.id; // is user autentif
 
     try {
         const [rows, fields] = await db.promise().query(`SELECT first_name, last_name, email, profile_pic FROM users WHERE id = ?`, [userId]);
@@ -32,7 +32,8 @@ exports.updateProfile = async (req, res) => {
 
     try {
         const query = `UPDATE users SET first_name = ?, last_name = ?, email = ? WHERE id = ?`;
-        await db.promise().query(query, [first_name, last_name, email, userId]);
+        const values = [first_name, last_name, email, userId]
+        await db.promise().query(query, values );
         res.status(200).json({success: true, message: 'Profile updated successfully'});
     } catch (error) {
         console.error('Error updating user profile:', error);
@@ -42,7 +43,7 @@ exports.updateProfile = async (req, res) => {
 
 exports.uploadProfilePic = async (req, res) => {
     const userId = req.user.id;
-    //const profilePicPath = req.file.path; // Calea către imaginea încărcată
+    //const profilePicPath = req.file.path; // path la img incarcata
     const profilePicUrl = `/uploads/${req.file.filename}`;
 
     console.log('Received file:', req.file); //fisier incarcat
@@ -62,26 +63,26 @@ exports.deleteAccount = async (req, res) => {
     const userId = req.user.id;
 
     try {
-        // Șterge favoritele utilizatorului
+        // sterg favorite
         const deleteFavoritesQuery = `DELETE FROM favorite WHERE user_id = ?`;
         await db.promise().query(deleteFavoritesQuery, [userId]);
 
-        // Șterge înregistrările din winners asociate cu userId ca winner_id
+        // sterg din winners row-urile asociate cu userId ca winner_id
         const deleteWinnersByWinnerQuery = `DELETE FROM winners WHERE winner_id = ?`;
         await db.promise().query(deleteWinnersByWinnerQuery, [userId]);
 
-        // Șterge înregistrările din winners asociate picturilor utilizatorului
+        // sterg inregistrarile din winners asociate picturilor utilizatorului
         const deleteWinnersByPaintingQuery = `DELETE FROM winners WHERE painting_id IN (SELECT id_painting FROM auction_paintings WHERE id_user = ?)`;
         await db.promise().query(deleteWinnersByPaintingQuery, [userId]);
 
-        // Șterge înregistrările din auctioneer asociate atât picturilor, cât și utilizatorului ca auctioneer
+        // sterg din auctioneer rowurile asociate atat picturilor cat si userului ca auctioneer
         const deleteAuctioneerByPaintingQuery = `DELETE FROM auctioneer WHERE id_painting IN (SELECT id_painting FROM auction_paintings WHERE id_user = ?)`;
         await db.promise().query(deleteAuctioneerByPaintingQuery, [userId]);
 
         const deleteAuctioneerByUserQuery = `DELETE FROM auctioneer WHERE id_auctioneer = ?`;
         await db.promise().query(deleteAuctioneerByUserQuery, [userId]);
 
-        // Șterge înregistrările din auction_paintings asociate utilizatorului
+        // sterg din auction_paintings rowurile asociate userului
         const deleteAuctionPaintingsQuery = `DELETE FROM auction_paintings WHERE id_user = ?`;
         await db.promise().query(deleteAuctionPaintingsQuery, [userId]);
         const query = `DELETE FROM users WHERE id = ?`;
